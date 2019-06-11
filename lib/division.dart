cf7a4d46a0a3efc62ca1db8441cff95e1d46eb96
Rein Gundersen Bentdal <rein.bent@gmail.com>
2019-06-12 00:05:15 +0200

    comment

--- lib/division.dart
+++ lib/division.dart
@@ -3,3 +3,17 @@ library division;
 export 'src/division.dart';
 export 'src/g.dart';
 export 'src/s.dart';
+
+//  Models for more robust code
+//
+//  # Example 
+//
+//  List<StyleItem> = [ S.margin(all: 10.0) ]
+//
+//  instead of
+//
+//  List<StyleItem> = [ S.margin(all: 10.0) ]
+//
+//  This prevents unnessesary errors with the wrong values inside a List
+export 'src/style_item.dart';
+export 'src/gesture_item.dart';
