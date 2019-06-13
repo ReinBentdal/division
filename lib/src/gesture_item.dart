2085ba78b878269ad41390d4f099bd53228badfc
Rein Gundersen Bentdal <rein.bent@gmail.com>
2019-06-13 16:58:31 +0200

    removed unused code

--- lib/src/gesture_item.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class GestureItem {
-  final String property;
-  final Function function;
-
-  GestureItem({@required this.property, @required this.function});
-}
--- lib/src/style_item.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class StyleItem {
-  final String property;
-  final dynamic style;
-
-  StyleItem({@required this.property, @required this.style});
-}
