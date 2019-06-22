0ce62c1a423e1564fa6e4c900a8c9bfa398d7868
Rein Gundersen Bentdal <rein.bent@gmail.com>
2019-06-22 21:38:11 +0200

    documentation fix

--- lib/src/class/gesture_class.dart
+++ lib/src/class/gesture_class.dart
@@ -5,13 +5,13 @@ import 'package:flutter/widgets.dart';
 /// ```dart
 /// Division(
 ///   style: StyleClass()
-///     ..width(100)
-///     ..height(150)
-///     ..borderRadius(all: 30.0)
-///     ..backgroundColor('#eeeeee'),
+///     .width(100)
+///     .height(150)
+///     .borderRadius(all: 30.0)
+///     .backgroundColor('#eeeeee'),
 ///   gesture: GestureClass()
-///     ..onTap(() => print('Widget pressed!'))
-///     ..onLongPress(() => print('Widget longpress)),
+///     .onTap(() => print('Widget pressed!'))
+///     .onLongPress(() => print('Widget longpress)),
 ///   child: Text('Some text),
 /// )
 /// ```
