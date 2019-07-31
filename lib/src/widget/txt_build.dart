c2fde72a000987dba9eec36bfc4251d540d0ba98
Rein Gundersen Bentdal <rein.bent@gmail.com>
2019-07-31 16:00:40 +0200

    minor tweak

--- lib/src/widget/txt_build.dart
+++ lib/src/widget/txt_build.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class TxtBuild extends StatelessWidget {
+class TxtBuild extends StatefulWidget {
 
   TxtBuild({
     @required this.text,
@@ -42,47 +42,83 @@ class TxtBuild extends StatelessWidget {
   final void Function(TextSelection, SelectionChangedCause) onSelectionChange;
 
   @override
+  _TxtBuildState createState() => _TxtBuildState();
+}
+
+class _TxtBuildState extends State<TxtBuild> {
+  String _text;
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    updateFocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _focusNode?.dispose();
+  }
+
+  void updateFocusNode() {
+    _focusNode = widget.focusNode;
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    if(_text == null)
+      _text = widget.text;
+
+    // if widget is changed from non editable to editable, the focusNode has to be added.
+    if(_focusNode == null)
+      updateFocusNode();
+
     final TextStyle _textStyle = TextStyle(
-        fontWeight: fontWeight,
-        fontSize: fontSize,
-        color: textColor,
-        fontStyle: fontStyle,
-        fontFamily: fontFamily,
-        fontFamilyFallback: fontFamilyFallback,
-        letterSpacing: letterSpacing,
-        wordSpacing: wordSpacing,
+        fontWeight: widget.fontWeight,
+        fontSize: widget.fontSize,
+        color: widget.textColor,
+        fontStyle: widget.fontStyle,
+        fontFamily: widget.fontFamily,
+        fontFamilyFallback: widget.fontFamilyFallback,
+        letterSpacing: widget.letterSpacing,
+        wordSpacing: widget.wordSpacing,
       );
 
     //  TODO: editable and non editable does not behave the same way. Known differences: 
     //  - Editable constraints is set to max, while non editable is not
 
-    if (editable == true) {
-      // TODO: Text resets when widgetTree is rebuilt
-      TextEditingController _controller = TextEditingController(text: text);
+    // TODO: Focus is added before rebuild. Focus point is therefore not correct.
+
+    if (widget.editable == true) {
+      TextEditingController _controller = TextEditingController(text: _text);
       // DefaultTextStyle _defaultTextStyle = DefaultTextStyle.of(context);
 
       return EditableText(
         cursorOpacityAnimates: true,
         style: _textStyle,
-        textAlign: textAlign,
-        maxLines: maxLines,
+        textAlign: widget.textAlign,
+        maxLines: widget.maxLines,
         controller: _controller,
-        focusNode: focusNode,
+        focusNode: _focusNode,
         backgroundCursorColor: Colors.grey,
         cursorColor: Colors.black,
-        keyboardType: keyboardType,
-        obscureText: obscureText,
-        onChanged: onChange,
-        onSelectionChanged: onSelectionChange,
+        keyboardType: widget.keyboardType,
+        obscureText: widget.obscureText,
+        onChanged: (value) {
+          _text = value;
+          widget.onChange(value);
+        },
+        onSelectionChanged: widget.onSelectionChange,
       );
     }
 
     return Text(
-      text,
+      _text,
       style: _textStyle,
-      textAlign: textAlign,
-      maxLines: maxLines,
+      textAlign: widget.textAlign,
+      maxLines: widget.maxLines,
     );
   }
 }
