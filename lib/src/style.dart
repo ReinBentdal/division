2c838c73416148430ea3c1f72d748e14f8245f36
ReinBentdal <rein.bent@gmail.com>
2020-04-14 15:58:14 +0200

    format

--- lib/src/style.dart
+++ lib/src/style.dart
@@ -389,7 +389,8 @@ class TxtStyle extends CoreStyle {
   void textDirection(TextDirection textDirection) =>
       _textModel?.textDirection = textDirection;
 
-  void textOverflow(TextOverflow textOverflow) => _textModel?.textOverflow = textOverflow;
+  void textOverflow(TextOverflow textOverflow) =>
+      _textModel?.textOverflow = textOverflow;
 
   void textShadow(
       {Color color = const Color(0x33000000),
@@ -486,9 +487,6 @@ class TxtStyle extends CoreStyle {
   TxtStyle clone() => TxtStyle(angleFormat: angleFormat)..add(this);
 
   TextModel get exportTextStyle => _textModel;
-
-
-
 }
 
 class Gestures {
